0b979474ea1c6d058f3685bfd8d9b98cd3b584b3
Ignacio Iglesias Castreño <ignacio.iglesias@hotmail.es>
2022-01-06 13:48:55 +0100

    feat: extract the eth2 network from the deposit data automatically

--- src/import_deposits.js
+++ src/import_deposits.js
@@ -3,10 +3,13 @@ require('dotenv').config({ path: path.join(__dirname, '../.env') })
 const fs = require('fs')
 const db = require('./db')
 
-const importPublicKeys = async (depositFile, network) => {
+const importPublicKeys = async (depositFile) => {
   // Read deposit array from file
   const rawDepositData = fs.readFileSync(depositFile)
   const depositData = JSON.parse(rawDepositData)
+
+  // Get the beacon chain network from the first element of the array
+  const network = depositData[0].eth2_network_name
 
   // Extract just the public keys
   const publicKeys = depositData.map((key) => key.pubkey)
@@ -14,19 +17,22 @@ const importPublicKeys = async (depositFile, network) => {
   // Get the current saved publicKeys in the db
   const savedPublicKeys = await db.query('SELECT public_key FROM beacon_chain_validators_monitoring')
 
+  // Get just the public_key field from the returned array
+  const savedPublicKeysMap = savedPublicKeys.map((key) => key.public_key)
+
   // Leave only the publicKeys that are not already saved
-  const newPublicKeys = publicKeys.filter(value => !savedPublicKeys.includes(value))
+  const newPublicKeys = publicKeys.filter(value => !savedPublicKeysMap.includes(value))
 
   // Save the new public keys with its Beaconchain endpoint in the db
   for (const newPublicKey of newPublicKeys) {
     await db.query('INSERT INTO beacon_chain_validators_monitoring (public_key, network) VALUES(?,?)', [newPublicKey, network])
   }
-  console.log('Process finished.', newPublicKeys.length, 'keys imported')
+  console.log('Process finished.', newPublicKeys.length, 'keys imported from', network)
 }
 
-// Usage: node src/import_deposits.js deposits/<deposit file> <network>
-if (process.argv[2] && process.argv[3]) {
-  importPublicKeys(process.argv[2], process.argv[3])
+// Usage: node src/import_deposits.js deposits/<deposit file>
+if (process.argv[2]) {
+  importPublicKeys(process.argv[2])
 } else {
-  console.error('Example usage: node src/import_deposits.js deposits/deposit_data-xxx.json gnosis')
+  console.error('Example usage: node src/import_deposits.js deposits/deposit_data-xxx.json')
 }
