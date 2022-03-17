0e6d174daaef67a50d6290ce089fa7bebf5ebe8b
Ignacio Iglesias Castreño <ignacio.iglesias@hotmail.es>
2022-03-17 12:27:22 +0100

    feat: send messages to different Discord channels depending on the network

--- src/discord_alerts.js
+++ src/discord_alerts.js
@@ -2,7 +2,7 @@ const { WebhookClient } = require('discord.js')
 
 const sendValidatorMessage = (alertType, publicKey, oldData, newData) => {
   // Log error message in the console
-  console.error(alertType, publicKey, oldData, newData)
+  console.log(alertType, publicKey, oldData, newData)
 
   // Create the Beaconchain direct link to the validator
   const network = process.argv[2]
@@ -11,7 +11,7 @@ const sendValidatorMessage = (alertType, publicKey, oldData, newData) => {
   // Prepare the text sent to Discord
   const text = `**${alertType}**\nNetwork: ${network}\n${oldData} 🡺 ${newData}\n[${publicKey}](<${beaconchainUrl}>)`
 
-  const webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_URL })
+  const webhookClient = new WebhookClient({ url: getDiscordWebhook(network) })
   webhookClient.send({
     username: 'Validator Monitoring Bot',
     content: text
@@ -20,18 +20,23 @@ const sendValidatorMessage = (alertType, publicKey, oldData, newData) => {
 
 const sendMessage = (alertType, message) => {
   // Log error message in the console
-  console.error(alertType, message)
+  console.log(alertType, message)
 
   const network = process.argv[2]
 
   // Prepare the text sent to Discord
   const text = `**${alertType}**\nNetwork: ${network}\nMessage:\n\n${message}`
 
-  const webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_URL })
+  const webhookClient = new WebhookClient({ url: getDiscordWebhook(network) })
   webhookClient.send({
     username: 'Validator Monitoring Bot',
     content: text
   })
+}
+
+// Gets the Discord webhook from the .env file
+const getDiscordWebhook = (network) => {
+  return process.env['DISCORD_WEBHOOK_URL_' + network.toUpperCase()]
 }
 
 module.exports = {
