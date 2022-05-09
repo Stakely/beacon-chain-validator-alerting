2c8b11a2586144f35ec64282bac5741852134355
Ignacio Iglesias Castreño <ignacio.iglesias@hotmail.es>
2022-05-09 21:32:13 +0200

    feat: improve discord logging

--- src/discord_alerts.js
+++ src/discord_alerts.js
@@ -9,12 +9,37 @@ const sendValidatorMessage = async (alertType, serverHostname, publicKey, oldDat
   const beaconchainUrl = process.env['BEACONCHAIN_ENDPOINT_' + network.toUpperCase()] + '/validator/' + publicKey + '#attestations'
 
   // Prepare the text sent to Discord
-  const text = `**${alertType}**\nServer hostname: ${serverHostname}\nNetwork: ${network}\n${oldData} 🡺 ${newData}\n[${publicKey}](<${beaconchainUrl}>)`
+  const title = `**${alertType}**`
+  const description = `Server hostname: ${serverHostname}\nNetwork: ${network}\n${oldData} 🡺 ${newData}\n[${publicKey}](<${beaconchainUrl}>)`
+
+  // Select a color depending on the importance
+  let color
+  if (alertType === 'BALANCE-DECREASING') {
+    color = 'ff9966' // Orange
+  } else if (alertType === 'SLASH-CHANGE') {
+    color = 'cc3300' // Red
+  } else if (alertType === 'STATUS-CHANGE') {
+    if (newData === 'active_online') {
+      color = '99cc33' // Green
+    } else if (newData === 'active_offline') {
+      color = 'ffcc00' // Yellow
+    }
+  }
+  // Default color as a backup (it shouldnt appear)
+  if (!color) {
+    color = '636264' // Gray
+  }
 
   const webhookClient = new WebhookClient({ url: getDiscordWebhook(network) })
   webhookClient.send({
     username: 'Validator Monitoring Bot',
-    content: text
+    embeds: [
+      {
+        title: title,
+        description: description,
+        color: color
+      }
+    ]
   })
 
   // Sleep 1 second to avoid rate limitting
@@ -28,16 +53,23 @@ const sendMessage = async (alertType, message) => {
   const network = process.argv[2]
 
   // Prepare the text sent to Discord
-  const text = `**${alertType}**\nNetwork: ${network}\nMessage:\n\n${message}`
+  const title = `**${alertType}**`
+  const description = `Network: ${network}\nMessage:\n${message}`
 
   const webhookClient = new WebhookClient({ url: getDiscordWebhook(network) })
   webhookClient.send({
     username: 'Validator Monitoring Bot',
-    content: text
+    embeds: [
+      {
+        title: title,
+        description: description,
+        color: 'ffcc00' // Yelow
+      }
+    ]
   })
 
   // Sleep 1 second to avoid rate limitting
-  await new Promise(resolve => setTimeout(resolve, 1000))
+  await new Promise(resolve => setTimeout(resolve, 2000))
 }
 
 // Gets the Discord webhook from the .env file
